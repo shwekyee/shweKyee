226a5eebac6f687a9b7c6168ce96da9026c6d154
Shwe Kyee <shwekyee.football@gmail.com>
2022-07-11 16:49:56 +0630

    first commit

---
+++ src/components/side/Side.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './side.css'
+import { ChatDotsFill, TelephoneFill } from 'react-bootstrap-icons'
+
+const Side = () => {
+  return (
+    <div className='side_bar'>
+        <a href='https://a3678.live4000.com/Chat/Chat?userID=&lang=en' className='bg-light text-dark'><ChatDotsFill></ChatDotsFill> Customer Support</a>
+        <a href='viber://chat/?number=%+9599960760366' className='side-viber'><TelephoneFill></TelephoneFill> Viber</a>
+    </div>
+  )
+}
+
+export default Side
